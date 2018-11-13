5bc2649767aa9d3e1b0ae421571023c9921d9a40
Kelly <devkells00@gmail.com>
2018-11-12 22:12:41 -0600

    Edit location and edit form rendering

--- src/Components/WaterZone/WaterMarker.js
+++ src/Components/WaterZone/WaterMarker.js
@@ -1,18 +1,72 @@
 import React, { Component } from 'react'
 import { Marker, InfoWindow } from "react-google-maps"
-
+import WaterLocForm from './WaterLocForm'
 
 class WaterMarker extends Component {
 
   state={
-    isOpen: false
-  } 
+    isOpen: false,
+    "geolocation": this.props.geolocation || "",
+    "pluscode": this.props.pluscode || "",
+    "city": this.props.city || "",
+    "country": this.props.country || "",
+    "image": this.props.image || null,
+    "details": this.props.details || "",
+    "active": this.props.active || "1",
+    "hours": this.props.hours || "",
+    "id": this.props.id || ""
+  }
+  
+  // onChange = e => {
+  //   const elementInState = e.target.id;
+  //   const value = e.target.value;
+  //   this.setState({ [elementInState]: value })
+  // }
 
   toggleOpen = () => {
     this.setState({ 
       isOpen: !this.state.isOpen
     });//to click and put fields into edit form, change this up to alter state to "selectedMarker", then if this marker matches the id of the marker.id, edit.
   }
+
+  // saveEdit= ({ geolocation, pluscode, city, country, image, details, active, hours }) => {
+  //   const token = localStorage.token
+  //   fetch(`http://localhost:3000/water_locations/${this.state.id}`, {
+  //   method: 'PATCH',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Authorization': `Bearer ${token}`
+  //   },
+  //   body: JSON.stringify ({
+  //     water_location: { 
+  //       geolocation, 
+  //       pluscode, 
+  //       city, 
+  //       country, 
+  //       image, 
+  //       details, 
+  //       active, 
+  //       hours
+  //     }
+  //   })})
+  //   .then(resp => resp.json())
+  //   .then(response => {
+  //     this.setState ({ 
+  //       mode: 'view',
+  //       geolocation: '', 
+  //       pluscode: '', 
+  //       city: '', 
+  //       country: '', 
+  //       image: '', 
+  //       details: '', 
+  //       active: '', 
+  //       hours: '',
+  //       id: ''
+  //     }, () => {
+  //       this.props.updateAllWaterLocs(response);
+  //     })
+  //   })
+  // }
 
   render(){
     let { lat, lng, waterloc } = this.props
@@ -22,17 +76,16 @@ class WaterMarker extends Component {
 
       {this.state.isOpen && // (this.props.selectedLocation === waterloc &&)
         <InfoWindow position={{lat, lng}} onCloseClick={this.onToggleOpen}>
-           <div>
-             <img src={waterloc.image_url}/>
-             <p>Geolocation: {waterloc.geolocation}</p>
-             <p>City: {waterloc.city}</p>
-             <p>Details: {waterloc.details}</p>
-             <p>Currently Active: {waterloc.active === 1 ? "No" : "Yes"}</p>
-             <p>Hours: {waterloc.hours}</p>
-             <button onClick={e => this.props.selectMarker(waterloc)}>Edit</button>
-           </div>
-         </InfoWindow>}
-       
+          <div>
+            <img src={waterloc.image_url} alt=""/>
+            <p>Geolocation: {waterloc.geolocation}</p>
+            <p>City: {waterloc.city}</p>
+            <p>Details: {waterloc.details}</p>
+            <p>Currently Active: {waterloc.active === 1 ? "No" : "Yes"}</p>
+            <p>Hours: {waterloc.hours}</p>
+            <button onClick={e => {this.props.selectMarker(waterloc); this.props.handleEdit()}}>Edit</button>
+          </div>
+        </InfoWindow>}
       </Marker>
       )
   }
--- src/Components/Wrapper.js
+++ src/Components/Wrapper.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Wrapper extends Component {
   render() {
     return (
-      <section id="four" class="wrapper style3">
-				<div class="inner">
-					<header class="align-center">
+      <section id="four" className="wrapper style3">
+				<div className="inner">
+					<header className="align-center">
 						<h2>Wrapper</h2>
 						<p>Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. </p>
 					</header>
